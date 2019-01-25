c23ff18eaf315715a61db9312ec521a25f797fbf
Baby-McBabyFace <32679064+Baby-McBabyFace@users.noreply.github.com>
2019-01-25 23:46:42 +0800

    Update progressBar.py

--- progressBar.py
+++ progressBar.py
@@ -14,3 +14,7 @@ def progressBar(countPerObject, totalNumberObject, waitTime, object=None):
 		percent = (i+1)
 		print("|{:s}{:s}|\t{:d}% Complete".format(progress, emptySpace, percent), end="\r", flush=True)
 	print()
+
+progressBar(5,20,0.05,"*")
+progressBar(1,50,0.04)
+progressBar(10,10,0.02)
